ededfef802fa637dcce301a16c30a380e2a24d67
NikitaAl05 <alexandrov.n05@icloud.com>
2026-04-23 23:50:20 +0400

    add Practice3 1-4

---
+++ Practices/Practice3.Task1/Student.cs
@@ -0,0 +1,10 @@
+namespace Practice3.Task1;
+
+class Student
+{
+    private string name = "Nikita";
+    private int age = 12;
+
+    public override string ToString() => $"name: {name}, age: {age}";
+    
+}
---
+++ Practices/Practice3.Task4/Book.cs
@@ -0,0 +1,32 @@
+using System.Text;
+
+namespace Practice3.Task4;
+
+class Book
+{
+    private string name;
+    private string author;
+    
+    public string Name { get; set; }
+    public string Author { get; set; }
+
+    public Book()
+    {
+        Name = "Джентльмены";
+        Author = "Гай Ричи";
+    }
+    public Book(string name, string author)
+    {
+        Name = name;
+        Author = author;
+    }
+    public string GetInfo()
+    {
+        var info = new StringBuilder();
+        info.AppendLine($"Name: {Name}");
+        info.AppendLine($"Author: {Author}");
+        
+        return info.ToString();
+    }
+    
+}
