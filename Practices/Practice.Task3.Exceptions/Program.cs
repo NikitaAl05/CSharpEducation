8f8c03085e5790252be4a875a44e0dacfb690319
NikitaAl05 <alexandrov.n05@icloud.com>
2026-07-24 12:58:20 +0400

    d

--- Practices/Practice.Task3.Exceptions/Program.cs
+++ Practices/Practice.Task3.Exceptions/Program.cs
@@ -1,9 +1,90 @@
-﻿namespace Practice.Task3.Exceptions;
+﻿using Practice4.Task2;
+
+namespace Practice.Task3.Exceptions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        
+        ConsoleLogger logger = new ConsoleLogger();
+        AgeScreen ageScreen = new AgeScreen(logger);
+
+        try
+        {
+            int attempts = 0;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Сколько вам лет?");
+                    int age = Convert.ToInt32(Console.ReadLine());
+                    ageScreen.AgeVerification(age);
+                    
+                    break;
+                }
+                catch (UnderageException ex)
+                {
+                    throw new TooManyAttemptsException(ex.Message);
+                }
+                catch (Exception ex) when (ex is ArgumentOutOfRangeException || 
+                                           ex is FormatException)
+                {
+                    attempts++;
+                    
+                    string errorText = ex is FormatException 
+                        ? "Ошибка: Вводить нужно только числа!" 
+                        : ex.Message;
+                    
+                    logger.Error(errorText);
+                
+                    logger.Info($"Предупреждение: Осталось попыток: {3 - attempts}");                
+                    if (attempts >= 3)
+                    {
+                        throw new TooManyAttemptsException("Доступ заблокирован: превышено число попыток.");
+                    }
+                }
+            }
+            Console.WriteLine("\n--- Проверка пройдена! ---");
+        }
+        catch (TooManyAttemptsException ex)
+        {
+            logger.Error($"[КРЯ] {ex.Message}");
+        }
+        Console.ReadLine();
     }
+}
+
+public class AgeScreen
+{
+    protected readonly ConsoleLogger _logger;
+
+    public AgeScreen(ConsoleLogger logger)
+    {
+        _logger = logger;
+    }
+    public void AgeVerification(int age)
+    {
+        if (age < 0 || age > 110)
+        {
+            _logger.Error($"Введен некорректный возраст: {age}");
+            throw new ArgumentOutOfRangeException(nameof(age), "Неверный диапазон возраста.");
+        }
+
+        if (age < 18)
+        {
+            throw new UnderageException("Ошибка: Вам нет 18 лет!");
+        }
+        
+        _logger.Info("Отлично! Ты уже взрослый!");
+    }
+}
+
+public class UnderageException : Exception
+{
+    public UnderageException(string message) : base(message) {}
+}
+
+public class TooManyAttemptsException : Exception
+{
+    public TooManyAttemptsException(string message) : base(message) {}
 }
---
+++ Practices/Practice.Task4.Exceptions/Program.cs
@@ -0,0 +1,54 @@
+﻿using Practice4.Task2;
+using System.Threading;
+
+namespace Practice.Task4.Exceptions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ConsoleLogger logger = new ConsoleLogger();
+
+        while (true)
+        {
+            try
+            {
+                Console.Write("Введите путь к файлу: ");
+                string path = Console.ReadLine();
+                
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string fileLine = reader.ReadLine();
+                        
+                        int file = Convert.ToInt32(fileLine);
+                        Console.WriteLine(fileLine);
+                    }
+                }
+                
+                Console.WriteLine("\n--- Файл успешно прочитан! ---");
+                break; 
+            }
+            catch (FileNotFoundException ex)
+            {
+                logger.Error($"Файл не найден: {ex.Message}");
+                Console.WriteLine("Ошибка: Указанный файл не существует. Попробуйте еще раз.\n");
+            }
+            catch (FormatException ex)
+            {
+                logger.Error($"В файле текст вместо чисел: {ex.Message}");
+                Console.WriteLine("Ошибка: В файле должны быть только числа. Исправьте файл и попробуйте снова.\n");
+            }
+            catch (IOException ex)
+            {
+                logger.Error($"Файл заблокирован: {ex.Message}");
+                Console.WriteLine("Ошибка: Файл открыт в другой программе. Ждем 3 секунды...\n");
+                
+                Thread.Sleep(3000); 
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
